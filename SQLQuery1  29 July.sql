22daf702e84f40f0b72778c6b782dc4a1b9ee51e
Ansh <147992981+anshuchoudhary1802@users.noreply.github.com>
2023-10-15 20:43:29 +0530

    SQL Queries

---
+++ SQLQuery1  29 July.sql
@@ -0,0 +1,16 @@
+use kanaujiya
+Select *from anshu
+CREATE TABLE Persons (ID int NOT NULL PRIMARY KEY,LastName varchar(255) NOT NULL,FirstName varchar(255), Age int)
+
+select *from Persons
+Insert into Persons values (1,'Sahu','vikas',11)
+INSERT iNto PeRsonS values (2,'Sahuu','vikass',111)
+INSERT iNto PeRsons values (3,'aSahuu','avikass',1)
+INsERT iNto PeRsons values (4,'asahuU','aviKass',1)
+INsERT iNto PeRsoNs values (5,'ahuU','ss',1)
+INsERT iNto PeRsoNs values (5,'anshuU','ss',1)
+select *from Persons where LastName='sahu'
+select *from Persons
+delete from persons where ID=1
+select *from persons
+
